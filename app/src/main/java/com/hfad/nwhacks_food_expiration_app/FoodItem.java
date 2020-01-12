9a80a13fd5f199c1878301c74c97b6355b012d9e
briellelaw <lawbrielle@gmail.com>
2020-01-12 11:42:39 -0800

    removed exceptions

--- app/src/main/java/com/hfad/nwhacks_food_expiration_app/FoodItem.java
+++ app/src/main/java/com/hfad/nwhacks_food_expiration_app/FoodItem.java
@@ -8,19 +8,19 @@ public class FoodItem {
     private int timeUntilExpire;
 
 
-    public FoodItem(String ForegroundC, String BackGroundC, List<String> dominantC, List<String> itemsInPicture, Set<String> validItems) throws InstantiationException {
+    public FoodItem(String ForegroundC, String BackGroundC, List<String> dominantC, List<String> itemsInPicture, Set<String> validItems) {
         this.itemType = assignItem(itemsInPicture, validItems);
         this.timeUntilExpire = expiryDate(ForegroundC, BackGroundC, dominantC, itemType);
 
     }
 
-    private String assignItem(List<String> itemsInPicture, Set<String> validItems) throws InstantiationException {
+    private String assignItem(List<String> itemsInPicture, Set<String> validItems) {
         for (String s : itemsInPicture) {
             if (validItems.contains(s)) {
                 return s;
             }
         }
-        throw new InstantiationException();
+        return "notValid";
     }
 
     private int expiryDate (String ForegroundC, String BackGroundC, List<String> dominantC, String itemType) {
